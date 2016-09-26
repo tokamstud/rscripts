0f53b84aceccb812c0258a02b6b6c6bd9d98f03e
tokams <t.gliniecki@hotmail.com>
2016-09-26 18:23:42 +0200

    task 1

--- ass1/montyhallproblem.R
+++ ass1/montyhallproblem.R
@@ -1,5 +1,27 @@
 ###--- Monty-Hall-problem
+# Auuthor: Tomasz gliniecki
 
-## probability to win by not swithcing
+keep <-0;
+switch <-0;
+possible_items <- c("car","goat0","goat1");
+possible_choices <- c(1,2,3);
 
-## probability to win by switching
+n <- 1e4;
+
+# calculate probability for winning with strategy 1 & 2
+for (i in c(0:n)) {
+    gate_distribution <- sample(possible_items, prob=c(1/3,1/3,1/3));
+    contestant_choice <- sample(possible_choices,size=1,prob=c(1/3,1/3,1/3));
+
+    # amount of wins strategy 1
+    if (gate_distribution[contestant_choice] == "car") {
+       keep <- keep+1;
+    }
+
+    # amount of wins strategy 2
+    if ("car" %in% gate_distribution[-contestant_choice]) {
+       switch <- switch+1;
+    }
+}
+print(keep/n);
+print(switch/n);
